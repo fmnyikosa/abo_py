59b9c7e9a2fcd4946eed355f888aab6b16c945f4
Favour M. Nyikosa <fmnyikosa@gmail.com>
2019-01-25 17:18:43 +0200

    update

--- 0.sh
+++ 0.sh
@@ -1,5 +1,5 @@
 # Wed Feb 16 14:00 2018 +0100
-export GIT_AUTHOR_DATE="2019-01-23  17:18:43 +0200"
-export GIT_COMMITTER_DATE="2019-01-23  17:18:43 +0200"
+export GIT_AUTHOR_DATE="2019-01-25  17:18:43 +0200"
+export GIT_COMMITTER_DATE="2019-01-25  17:18:43 +0200"
 git add *
 git commit -m "update"
